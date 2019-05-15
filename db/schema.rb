12d8a741a81d8c435c671726177d0d51f3c21b4e
maxwellbenton <maxwellbenton@gmail.com>
2019-05-15 09:26:07 -0400

    resets to starter

--- db/schema.rb
+++ db/schema.rb
@@ -19,20 +19,4 @@ ActiveRecord::Schema.define(version: 2019_05_14_104321) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "locations", force: :cascade do |t|
-    t.float "latitude"
-    t.float "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "sightings", force: :cascade do |t|
-    t.integer "bird_id"
-    t.integer "location_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["bird_id"], name: "index_sightings_on_bird_id"
-    t.index ["location_id"], name: "index_sightings_on_location_id"
-  end
-
 end
