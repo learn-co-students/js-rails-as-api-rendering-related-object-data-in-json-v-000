f365a2ba132eb6570b1e5f16234a1fe0e0afaf37
maxwellbenton <maxwellbenton@gmail.com>
2019-05-21 07:41:41 -0400

    updates solution

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,6 @@
 class SightingsController < ApplicationController
     def index
-        @sightings = Sighting.all
-        render json: @sightings.to_json(include: [:bird, :location])
+        sightings = Sighting.all
+        render json: sightings.to_json(include: [:bird, :location])
       end
 end
