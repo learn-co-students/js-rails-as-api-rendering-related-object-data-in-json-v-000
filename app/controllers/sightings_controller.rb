8f911159eb9eb5ffbdcaa9a9d2502a77322d3f77
github username <github email address>
2019-10-10 17:50:22 -0400

    rendering related object data

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,20 @@
+class SightingsController < ApplicationController
+  def index
+    sightings = Sighting.all
+    render :json => sighting, :include => [:bird, :location]
+  end
+
+  def show
+    sighting = Sighting.find_by(:id => params[:id])
+    # render :json => sighting
+
+    # render :json => { :id => sighting.id, :bird => sighting.bird, :location => sighting.location }
+    # render :json => sighting, :include => [:bird, :location]
+
+    if sighting
+      render :json => sighting, :include =>  [:bird, :location], :except => [:updated_at]
+    else
+      render :json => { :message => 'No sighting found with that id' }
+    end
+  end
+end
