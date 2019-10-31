37854bc9bc528b83347cf1d49fd52635b0f53a2c
Sarah Paz <sarah.paz@gmail.com>
2019-10-31 12:18:48 -0400

    Done.

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,18 @@
+class SightingsController < ApplicationController
+	def index
+		sightings = Sighting.all
+		# render json: sightings, include: [:bird, :location]
+		render json: sightings.to_json(include: [:bird, :location])
+	end
+
+	def show
+		sighting = Sighting.find_by(id: params[:id])
+		if sighting
+		# render json: { id: sighting.id, bird: sighting.bird, location: sighting.location}
+		# render json: sighting, include: [:bird, :location]
+			render json: sighting.to_json(include: [:bird, :location])
+		else
+			render json: { message: 'No sighting found with that id' }
+		end
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get '/birds' => 'birds#index'
+  resources :sightings
+	get '/birds' => 'birds#index'
+	
 end
