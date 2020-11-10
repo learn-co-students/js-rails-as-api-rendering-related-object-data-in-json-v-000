f0a169467e0654c9c8c4706cd0d712025dd13887
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2020-11-10 11:44:32 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_104321) do
+ActiveRecord::Schema.define(version: 2020_11_10_160505) do
 
   create_table "birds", force: :cascade do |t|
     t.string "name"
     t.string "species"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sightings", force: :cascade do |t|
+    t.integer "bird_id"
+    t.integer "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bird_id"], name: "index_sightings_on_bird_id"
+    t.index ["location_id"], name: "index_sightings_on_location_id"
   end
 
 end
